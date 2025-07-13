bf7e22bc5963fbccf4903c27bce486cde167c488
gmsalvaro <ajsgomes24@gmail.com>
2025-07-13 13:27:19 -0300

    Ajustes Finais

--- sistemaMatricula/src/test/java/SistemaMatriculaMultiplosTest.java
+++ sistemaMatricula/src/test/java/SistemaMatriculaMultiplosTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SistemaMatriculaTestMultiplos {
+class SistemaMatriculaMultiplosTest {
 
     private SistemaMatricula sistema;
     private Aluno alunoTeste;
